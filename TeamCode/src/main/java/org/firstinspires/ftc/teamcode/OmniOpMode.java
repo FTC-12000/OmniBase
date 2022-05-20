2f060b291463498ab05de87f4808e8c271e6bd2d
laric526 <laric526@hsd.k12.or.us>
2022-05-19 17:16:41 -0700

    Removed unused imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpMode.java
@@ -29,12 +29,9 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Omni OpMode", group="12000")
