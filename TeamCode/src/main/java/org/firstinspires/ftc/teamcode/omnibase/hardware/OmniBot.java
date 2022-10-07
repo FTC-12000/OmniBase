5572fdbbd192d9f6c6e43b077ed73fd4dbb82117
laric526 <laric526@hsd.k12.or.us>
2022-10-06 17:45:13 -0700

    Added Javadoc to OmniBot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/omnibase/hardware/OmniBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/omnibase/hardware/OmniBot.java
@@ -3,12 +3,21 @@ package org.firstinspires.ftc.teamcode.omnibase.hardware;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+/**
+ * A wrapper for an OmniBot's motors that adds support for custom motor names.
+ * @author Calvin Larimore
+ */
 public class OmniBot {
     private final DcMotor frontRightDrive;
     private final DcMotor frontLeftDrive;
     private final DcMotor backRightDrive;
     private final DcMotor backLeftDrive;
 
+    /**
+     * Creates a new OmniBot using default motor names (formatted like "front_right_drive").
+     * For custom motors see {@link OmniBot#OmniBot(DcMotor, DcMotor, DcMotor, DcMotor)}.
+     * @param hardwareMap The {@link HardwareMap} to get the motors from
+     */
     public OmniBot(HardwareMap hardwareMap) {
         this(
                 hardwareMap.get(DcMotor.class, "front_right_drive"),
@@ -18,6 +27,14 @@ public class OmniBot {
         );
     }
 
+    /**
+     * Creates a new OmniBot using the motors passed to it.
+     * For default motors see {@link OmniBot#OmniBot(HardwareMap)}.
+     * @param frontRightDrive The front right drive motor
+     * @param frontLeftDrive The front left drive motor
+     * @param backRightDrive The back right drive motor
+     * @param backLeftDrive The back left drive motor
+     */
     public OmniBot(DcMotor frontRightDrive, DcMotor frontLeftDrive, DcMotor backRightDrive, DcMotor backLeftDrive) {
         this.frontRightDrive = frontRightDrive;
         this.frontLeftDrive = frontLeftDrive;
@@ -30,15 +47,46 @@ public class OmniBot {
         backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
     }
 
+    /**
+     * Sets the power of the front right drive motor
+     * @param power The new power level (0-1)
+     * @see OmniBot#setFrontLeftDrivePower(double)
+     * @see OmniBot#setBackRightDrivePower(double)
+     * @see OmniBot#setBackLeftDrivePower(double)
+     */
     public void setFrontRightDrivePower(double power) {
         frontRightDrive.setPower(power);
     }
+
+    /**
+     * Sets the power of the front left drive motor
+     * @param power The new power level (0-1)
+     * @see OmniBot#setFrontRightDrivePower(double)
+     * @see OmniBot#setBackRightDrivePower(double)
+     * @see OmniBot#setBackLeftDrivePower(double)
+     */
     public void setFrontLeftDrivePower(double power) {
         frontLeftDrive.setPower(power);
     }
+
+    /**
+     * Sets the power of the back right drive motor
+     * @param power The new power level (0-1)
+     * @see OmniBot#setFrontRightDrivePower(double) 
+     * @see OmniBot#setFrontLeftDrivePower(double)
+     * @see OmniBot#setBackLeftDrivePower(double)
+     */
     public void setBackRightDrivePower(double power) {
         backRightDrive.setPower(power);
     }
+
+    /**
+     * Sets the power of the back left drive motor
+     * @param power The new power level (0-1)
+     * @see OmniBot#setFrontRightDrivePower(double)
+     * @see OmniBot#setFrontLeftDrivePower(double)
+     * @see OmniBot#setBackRightDrivePower(double) 
+     */
     public void setBackLeftDrivePower(double power) {
         backLeftDrive.setPower(power);
     }
